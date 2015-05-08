ab0b261618007bd61058c0e6acbecb4da918bbc4
Pepo48 <pepo48@gmail.com>
2015-05-08 17:28:19 +0200

    Added some meaningful stuff- whole functionality in TextMain class transformed to GUI and tested _______________________________________________ Minor observation: output .ods reports some error, when it's open in Excel, Excel runs some self error corrections and finally, the file is displayed correctly; on Ubuntu with OpenOffice everything works perfectly

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,35 +1,81 @@
 package sample;
 
+import cz.muni.fi.pb138.kartoteka.entities.Category;
+import cz.muni.fi.pb138.kartoteka.exceptions.CategoryException;
+import cz.muni.fi.pb138.kartoteka.loaders.FileManager;
+import cz.muni.fi.pb138.kartoteka.loaders.FileManagerImpl;
+import cz.muni.fi.pb138.kartoteka.managers.KartotekaManager;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 /**
  * Created by Peter Zaoral on 8.5.2015.
  */
 public class Controller implements Initializable {
+    FileManager fm = new FileManagerImpl();
+    ObservableList<String> list = FXCollections.observableArrayList();
+    KartotekaManager kart;
 
     @FXML
     private ComboBox<String> comboBox;
     @FXML
-    private Label label;
+    private Label statusLabel;
     @FXML
-    private void buttonAction(ActionEvent event)
+    private TextField addCategoryTextField;
+    @FXML
+    public Label label;
+    @FXML
+    public Button saveButton;
+    @FXML
+    public Button addButton;
+    @FXML
+    public void addButtonAction(ActionEvent event)
     {
-        label.setText("Selected value "+ comboBox.getValue());
+        try {
+            Category category = new Category(addCategoryTextField.getText());
+            kart.addCategory(category);
+            list.add(category.getName());
+            comboBox.setItems(list);
+        } catch (CategoryException e) {
+            e.printStackTrace();
+        }
+
     }
-    ObservableList<String> list = FXCollections.observableArrayList("Value-1","Value-2","Value-3","Value-4","Value-5","Value-6");
+    @FXML
+    private void saveChangesAction(ActionEvent event)
+    {
+        try {
+            fm.save("dokument.ods", kart);
+            statusLabel.setText("Status: Changes saved");
+        } catch (Exception e) {
+            e.printStackTrace();
+            statusLabel.setText("Status: Exception was thrown during saving. Try again.");
+        }
+    }
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        try {
+            kart = fm.load("dokument.ods");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (kart!= null )
+        {
+            list.addAll(kart.getCategories().stream().map(Category::getName).collect(Collectors.toList()));
+        }
         comboBox.setItems(list);
     }
 }
